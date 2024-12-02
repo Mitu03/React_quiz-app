ea796a3a95fe463ce05144477c1df94ccccb8bfe
Mitu kr Rabha <166889598+Mitu03@users.noreply.github.com>
2024-12-02 08:58:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,130 @@
+/* eslint-disable no-undef */
+import Header from "./Header";
+import Loader from "./Loader";
+import Error from "./Error";
+import Main from "./Main";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import { useEffect, useReducer } from "react";
+import FinishScreen from "./FinishScreen";
+
+const initialState = {
+  questions: [],
+  //'loading, 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceive":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFail":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+    case "restart":
+      return { ...initialState, question: state.questions, status: "ready" };
+    // return {
+    //   ...state,
+    //   points: 0,
+    //   highScore: 0,
+    //   index: 0,
+    //   answer: null,
+    //   status: "ready",
+    // };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+  const maxPosssiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceive", payload: data }))
+      .catch((err) => dispatch({ type: "dataFail" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestion={numQuestions}
+              points={points}
+              maxPosssiblePoints={maxPosssiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
+          </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPosssiblePoints}
+            highScore={highScore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
